2a23354eaf29d4923a814412546ae111d5b621b7
bingo2011 <bin.go2011@yahoo.com>
2019-02-15 12:16:57 +0800

    can see logging info

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "rtc_base/logging.h"
 #include <iostream>
 
+using namespace rtc;
 int main()
 {
+    LogMessage::LogToDebug(LS_VERBOSE);
     RTC_LOG(LS_VERBOSE) << "Hello WebRTC";
     return 0;
 }
